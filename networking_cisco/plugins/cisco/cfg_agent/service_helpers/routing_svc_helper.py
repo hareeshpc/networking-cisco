0d1b735d88bf7a085513b502b96a198c8d8c6cb1
Hareesh Puthalath <hareesh.puthalath@gmail.com>
2015-08-18 17:50:07 +0200

    Changes for adding a OVS ML2 based plugging driver for CSR
    This commit introduces a new ml2_ovs_plugging_driver which makes use
    of VIF hotplugging in CSR to support the 'router-interface-add' API
    action.
    
    * The service_vm_lib has been modified for supporting interface attach and
    detach operations.
    
    * Changes to the csr routing driver
    Added new csr1kv hotplug routing driver with external net support, added new
    snippets and tweaked the original csr1kv routing driver.
    
    * Unit tests for the ml2_ovs_plugging driver
    
    * Changes for working in the networking-cisco repo
    
    Change-Id: I7252bbd787dd47d6b66acff47056333efd63ab92

--- networking_cisco/plugins/cisco/cfg_agent/service_helpers/routing_svc_helper.py
+++ networking_cisco/plugins/cisco/cfg_agent/service_helpers/routing_svc_helper.py
@@ -386,7 +386,7 @@ class RoutingServiceHelper(object):
                 except cfg_exceptions.DriverException as e:
                     LOG.exception(_LE("Driver Exception on router:%(id)s. "
                                     "Error is %(e)s"), {'id': r['id'], 'e': e})
-                    self.updated_routers.update(r['id'])
+                    self.updated_routers.update([r['id']])
                     continue
             # identify and remove routers that no longer exist
             for router_id in prev_router_ids - cur_router_ids:
@@ -451,7 +451,7 @@ class RoutingServiceHelper(object):
             self._routes_updated(ri)
         except cfg_exceptions.DriverException as e:
             with excutils.save_and_reraise_exception():
-                self.updated_routers.update(ri.router_id)
+                self.updated_routers.update([ri.router_id])
                 LOG.error(e)
 
     def _process_router_floating_ips(self, ri, ex_gw_port):
